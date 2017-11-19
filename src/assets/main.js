52c5563cfeb9f891a076f487f13ea07137a9ffd1
Volodymyr Padovsky <vld06@ukr.net>
2017-11-20 01:08:39 +0200

    Fixing 4

--- src/assets/main.js
+++ src/assets/main.js
@@ -52,7 +52,7 @@ function setMessage(message){
 
 // 10 Create validateInput function
 function validateInput(input){
-	if(param.length !== 4){
+	if(input.length !== 4){
 		setMessage("Guesses must be exactly 4 characters long.");
 		return false;
 	}
